6662a6007c5e3685ca40509d65c3131a8fbf87f4
truong <anhtruong01226634086@gmail.com>
2022-11-14 14:20:32 +0700

    build-container

--- src/components/Home/Container/Container.js
+++ src/components/Home/Container/Container.js
@@ -1,5 +1,79 @@
-function Container({ children }) {
-    return <div style={{ marginTop: '130px' }}>{children}</div>;
+import classNames from 'classnames/bind';
+import styles from './Container.module.scss';
+import bg1 from '~/assets/background/backgroundHome.jpg';
+import bg2 from '~/assets/background/backgroundHome2.jpg';
+import bg3 from '~/assets/background/home3.jpg';
+import bg4 from '~/assets/background/home4.jpg';
+import bg5 from '~/assets/background/home5.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { IconCircle } from '~/components/Icons/Icons';
+import Image from '../Image';
+
+const cx = classNames.bind(styles);
+
+function Container() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('image')}>
+                <Image className={cx('image-bg')} src={bg1} />
+            </div>
+            <div className={cx('image')}>
+                <div className={cx('image-box')}>
+                    <div className={cx('image-col-image')}>
+                        <Image src={bg2} className={cx('image-bg')} />
+                    </div>
+                    <div className={cx('image-col-title')}>
+                        <h6>ABOUT OUR COLLECTIONS</h6>
+                        <h2>Experience new way of designing</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adi piscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Enim ut sem viverra aliquet eget sit amet.
+                        </p>
+                        <div className={cx('image-col-icon-box')}>
+                            <IconCircle className={cx('image-col-icon')} />
+
+                            <span className={cx('image-col-icon-btn')}>Read More</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={cx('image-box')}>
+                    <div className={cx('image-col-title')}>
+                        <h6>ABOUT OUR SHOP</h6>
+                        <h2>Experience the shop</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adip iscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Enim ut sem viverra aliquet eget sit amet nunc.
+                        </p>
+                    </div>
+                    <div className={cx('image-col-image1')}>
+                        <Image src={bg3} className={cx('image-bg1')} />
+                    </div>
+                </div>
+                <div className={cx('image-box')}>
+                    <div className={cx('image-col-image1')}>
+                        <Image src={bg4} className={cx('image-bg1')} />
+                    </div>
+                    <div className={cx('image-col-title')}>
+                        <h6>ABOUT USED MATERIALS</h6>
+                        <h2>Experience wood-work items</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adi piscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Enim ut sem viverra aliquet eget sit amet nunc.
+                        </p>
+                        <div className={cx('image-col-icon-box')}>
+                            <IconCircle className={cx('image-col-icon')} />
+
+                            <span className={cx('image-col-icon-btn')}>Read More</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('image')}>
+                <Image className={cx('image-bg')} src={bg5} />
+            </div>
+        </div>
+    );
 }
 
 export default Container;
--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -1,5 +1,68 @@
+import classNames from 'classnames/bind';
+import styles from './Footer.module.scss';
+import footer1 from "~/assets/images/footer-logo-group.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightDots, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import Image from '../Image';
+
+const cx = classNames.bind(styles);
 function Footer({ children }) {
-    return <div>{children}</div>;
+    return (
+        <div className={cx('wrapper')}>
+        <div className={cx(['container'])}>
+        <div className={cx('footer')}>
+                <h3> Umera</h3>
+                <p>
+                Welcome to a place of refinement and beauty. This is Umeå, a WP gem we made for all furniture stores & brands.
+                </p>
+                <div className={cx('footer-widget')}>
+                    <h6>FB.</h6>
+                    <h6>TW.</h6>
+                    <h6>IN.</h6>
+                    <h6>BE.</h6>
+                    <h6>DR.</h6>
+
+                </div>
+
+            </div>
+            <div className={cx('footer')}>
+                <h3> Studio</h3>
+                <li>Visit our Store  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+                <li>About Us  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+                <li>Our Blog  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+                <li>Craftsmanship  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+                <li>Apply For a Job  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+
+            </div>
+            <div className={cx('footer')}>
+                <h3> Shopping </h3>
+                <li>Online Payments  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+                <li>Gift Cards  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+                <li>Return Policy  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+                <li>Furniture Assembling  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+                <li>Shipping Methods  <FontAwesomeIcon icon={faArrowUpRightFromSquare} className={cx('footer-icon')} />  </li>
+
+            </div>
+            <div className={cx('footer')}>
+                <h3> Payment Methods</h3>
+                <p>
+                Select one of many supported payment providers from the list below.
+                </p>
+                <div  className={cx('footer-widget')}> 
+                <Image src={footer1} />
+
+                </div>
+
+            </div>
+
+        </div>
+        <div className={cx(['container'])}>
+            <p className={cx(['container-overight'])} >© 2021 Qode Interactive, All Rights Reserved</p>
+
+        </div>
+
+        </div>
+    )
 }
 
 export default Footer;
